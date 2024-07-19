5678cbd4a5ffd5d570cd0799412686560b4d2162
aquibk580 <aquibk580@gmail.com>
2024-07-19 20:11:22 +0530

    changes in db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,26 @@
 import mongoose from "mongoose";
-import colors from 'colors';
-import dotenv from 'dotenv';
+import colors from "colors";
+import dotenv from "dotenv";
 dotenv.config();
 
 const URI = process.env.MONGOURI;
 
 const connectToMongo = async () => {
   try {
-    const connection = await mongoose.connect(URI)
-    console.log(`Connected to mongoDB Successfully ${connection.connection.host}`.bgMagenta.white);
+    const connection = await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, 
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log(
+      `Connected to MongoDB Successfully: ${connection.connection.host}`
+        .bgMagenta.white
+    );
   } catch (error) {
-    console.log(`Error in Mongodb ${error}`.bgRed.white)
+    console.log(`Error in MongoDB Connection: ${error.message}`.bgRed.white);
   }
 };
 
 export default connectToMongo;
-
-
